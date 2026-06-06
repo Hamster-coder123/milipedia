c180a056aa8b6832f2c54c4b885dc250b972bd88
Hamster-coder123 <hamadan.bilal@gmail.comm>
2026-06-07 01:18:47 +0200

    Bust F-16 template cache

--- js/aircraft-page.js
+++ js/aircraft-page.js
@@ -500,7 +500,7 @@ function renderArticle(item) {
 const STATIC_ARTICLE_TEMPLATES = {
   "f-16-fighting-falcon": {
     title: "General Dynamics F-16 Fighting Falcon - Milipedia",
-    path: "data/f16-template.html"
+    path: "data/f16-template.html?v=public-perception"
   },
   "f-4-phantom-ii": {
     title: "McDonnell Douglas F-4 Phantom II - Milipedia",
